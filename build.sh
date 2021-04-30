73d044c5589c7df2a1c24f36c71039d294936d0e
Shukri Adams <shukri.adams@gmail.com>
2021-04-30 22:31:56 +0200

    arm flag now set from script, bash lookup failed on x86

--- build.sh
+++ build.sh
@@ -1,31 +1,34 @@
 set -e
 
 DOCKERPUSH=0
+ARCHITECTURE=""
 while [ -n "$1" ]; do 
     case "$1" in
-    --dockerpush) DOCKERPUSH=1 ;;
+    --dockerpush) 
+        DOCKERPUSH=1 ;; 
+    --arc)
+        ARCHITECTURE="$2" shift;;      
     esac 
     shift
 done
 
 docker build -t shukriadams/node12build .
+echo "container built"
 
-#test 
 LOOKUP=$(docker run shukriadams/node12build:latest bash -c "node -v") 
 if [ "$LOOKUP" != "v12.22.1" ] ; then
     echo "ERROR : container returned unexpected string ${LOOKUP}"
     exit 1
+else
+    echo "container smoketest passed"
 fi
 
 if [ $DOCKERPUSH -eq 1 ]; then
+    echo "starting docker push"
     TAG=$(git describe --tags --abbrev=0) 
-    ARMCHECK=$(lscpu|grep arm)
-    ARCHITECTURE=""
-    if [ ! -z "$ARMCHECK" ]; then
-        ARCHITECTURE="-arm"
-    fi
+    echo "Tag ${TAG} detected"
 
-    docker tag shukriadams/node12build:latest shukriadams/node12build:$TAG$ARCHITECTURE
+    docker tag shukriadams/node12build:latest shukriadams/node12build:"${TAG}${ARCHITECTURE}"
     docker login -u $DOCKER_USER -p $DOCKER_PASS 
     docker push shukriadams/node12build:$TAG$ARCHITECTURE
 fi
